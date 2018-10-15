90edcc0ddce44172577a6d887383754d08055018
sulistiyanto <sulistiyanto404@gmail.com>
2018-10-15 16:16:09 +0700

    Initial Commit

---
+++ UITabBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UITabBar
+//
+//  Created by User on 15/10/18.
+//  Copyright © 2018 JYSK Indonesia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
